b7d736f75c177f07d2d24d0ac0ef44f7551f728b
MohamedTariq511990 <727722euit511@skcet.ac.in>
2023-02-24 10:42:43 +0530

    first commit

---
+++ src/main/java/com/example/springapp/Controller/ApiController.java
@@ -0,0 +1,29 @@
+package com.example.springapp.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springapp.Model.Employee;
+import com.example.springapp.Service.ApiService;
+
+
+@RestController
+public class ApiController {
+
+	@Autowired
+	ApiService stuService;
+	@PostMapping("/addDetails")
+	public Employee addinfo(@RequestBody Employee st) {
+		return stuService.saveDetails(st);
+	}
+	@GetMapping("/showDetails")
+	public List<Employee> fetchDetails()
+	{
+		return stuService.getDetails();
+	}
+}
